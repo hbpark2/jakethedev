2e8b986ecc852ebe62b3c22dc3fcdd25d315c64c
hbpark2 <64389093+hbpark2@users.noreply.github.com>
2022-04-02 17:55:06 +0900

    redirect

--- src/Components/Routes/Routes.tsx
+++ src/Components/Routes/Routes.tsx
@@ -1,4 +1,4 @@
-import { Route, Switch } from "react-router";
+import { Route, Switch, Redirect } from "react-router";
 import Detail from "../../Detail/Detail";
 import Resume from "../../Home/components/Resume/Resume";
 import Home from "../../Home/Home";
@@ -11,6 +11,7 @@ const Routes = () => {
       <Route exact path="/" component={Home} />
       <Route exact path="/resume" component={Resume} />
       <Route exact path="/detail/:id" component={Detail} />
+      <Redirect to="/" />
     </Switch>
   );
 };
